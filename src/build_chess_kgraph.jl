02d0b1996585e7b14d54846d24ee9359e107e8bf
Naseer Dari <naseerdari01@gmail.com>
2019-10-07 15:10:06 -0400

    fix the extraction

--- src/build_chess_kgraph.jl
+++ src/build_chess_kgraph.jl
@@ -1,3 +1,10 @@
-using CSV, DataFrames
+using CSV, DataFrames, DataFramesMeta
 
 df = DataFrame(CSV.read("data/chess.csv"))
+
+x = let x = Dict{String, String}()
+  @byrow! df begin
+    x[:title] = :abstract
+  end
+  x
+end
--- src/extract_chess_info.jl
+++ src/extract_chess_info.jl
@@ -8,13 +8,12 @@ using DataFramesMeta
 
 @main function exec(raw_csv::AbstractString)
   df = DataFrame(CSV.read(raw_csv))
-  res = filter(i -> chess_data(i), df)
+  res = @where df chess_data.(:abstract)
   fixed = @byrow! res :title = replace(:title, "Wikipedia: " => "")
-  fixed2 = @byrow! fixed :title = replace(:title, r" \((.*)\)" => "")
-  CSV.write("../data/chess.csv", fixed2)
+  CSV.write("../data/chess.csv", fixed)
 end
 
 function chess_data(x)
-  typeof(x.abstract) == String ? occursin(r" (C|c)hess(\s|\.)", x.abstract) : false
+  typeof(x) == String ? occursin(r" (C|c)hess(\s|\.)", x) : false
 end
 
