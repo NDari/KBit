d212190329a0b1c2132c42657aabc11e7b3bf0c3
Naseer Dari <naseerdari01@gmail.com>
2019-10-09 09:58:08 -0400

    start work on chess kgraph

--- src/build_chess_kgraph.jl
+++ src/build_chess_kgraph.jl
@@ -1,6 +1,13 @@
 using CSV, DataFrames, DataFramesMeta
 
 df = DataFrame(CSV.read("data/chess.csv"))
+keys = Dict{String, Array{String, 1}}()
+for i in df.title
+	s = replace(i, r" \(.*\)" => "")
+  res = @where df occursin.(s, :abstract)
+  res2 = @byrow! res :title = replace(:title, r" \(.*\)" => "")
+	keys[s] = String.(res2.title)
+end
 
 x = let x = Dict{String, String}()
   @byrow! df begin
