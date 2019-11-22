bf18442b69d51dcbaa52a086d92dc0dbe7dcfd45
Naseer Dari <naseerdari01@gmail.com>
2019-11-21 22:54:24 -0500

    starting on text section

--- src/extract_articles.jl
+++ src/extract_articles.jl
@@ -13,69 +13,63 @@ function extract(file)
 end
 
 function extract_page(stream)
-    nf = "<mediawiki>\n"
-    page_found = false
+    nf = ""
+    text_found = false
+    text_regex = r"<text xml:space=\"preserve\">"
     title_regex = r"<title>(.*)</title>"
-    title = nothing
     for line in eachline(stream)
-        if page_found
+        if !text_found
+            if occursin(text_regex, line)
+                nf *= replace(line, text_regex => "") * "\n"
+                text_found = true
+            end
             if occursin(title_regex, line)
                 title = match(title_regex, line)[1]
-                title = replace(title, r"/| " => "_")
+                nf *= "# " * replace(title, r"/| " => "_") * "\n"
             end
-            if occursin(r"</page>", line)
-                nf *= line * "\n"
-                break
-            end
-            nf *= line * "\n"
             continue
         end
-        if !page_found && occursin(r"<page>", line)
-            nf *= line * "\n"
-            page_found = true
+        if occursin(r"</text>", line)
+            nf *= replace(line, r"</text>" => "") * "\n"
+            break
         end
+        nf *= line * "\n"
     end
-    if nf === "<mediawiki>/n"
-        return
-    end
-    nf *= "</mediawiki>"
-    f = open("./data/$title.xml", "w")
-    write(f, nf)
-    close(f)
+    return nf
 end
 
 
-function par_extract_page(mmap_file)
-    chunck_size = length(mmap_file) ÷ nthreads()
-    idx_start = threadid() === 1 ? 1 : chunck_size * (threadid() - 1)
-    idx_end = threadid() === nthreads() ? length(mmap_file) : chunck_size * threadid()
-    stream = TranscodingStream(Bzip2Decompressor(), IOBuffer(fs[idx_start:idx_end]))
-    nf = "<mediawiki>\n"
-    page_found = false
-    title_regex = r"<title>(.*)</title>"
-    title = nothing
-    for line in eachline(stream)
-        if page_found
-            if occursin(title_regex, line)
-                title = match(title_regex, line)[1]
-            end
-            if occursin(r"</page>", line)
-                nf *= line * "\n"
-                break
-            end
-            nf *= line * "\n"
-            continue
-        end
-        if !page_found && occursin(r"<page>", line)
-            nf *= line * "\n"
-            page_found = true
-        end
-    end
-    nf *= "</mediawiki>"
-    f = open("./data/$title.xml", "w")
-    write(f, nf)
-    close(f)
-end
+# function par_extract_page(mmap_file)
+#     chunck_size = length(mmap_file) ÷ nthreads()
+#     idx_start = threadid() === 1 ? 1 : chunck_size * (threadid() - 1)
+#     idx_end = threadid() === nthreads() ? length(mmap_file) : chunck_size * threadid()
+#     stream = TranscodingStream(Bzip2Decompressor(), IOBuffer(fs[idx_start:idx_end]))
+#     nf = "<mediawiki>\n"
+#     page_found = false
+#     title_regex = r"<title>(.*)</title>"
+#     title = nothing
+#     for line in eachline(stream)
+#         if page_found
+#             if occursin(title_regex, line)
+#                 title = match(title_regex, line)[1]
+#             end
+#             if occursin(r"</page>", line)
+#                 nf *= line * "\n"
+#                 break
+#             end
+#             nf *= line * "\n"
+#             continue
+#         end
+#         if !page_found && occursin(r"<page>", line)
+#             nf *= line * "\n"
+#             page_found = true
+#         end
+#     end
+#     nf *= "</mediawiki>"
+#     f = open("./data/$title.xml", "w")
+#     write(f, nf)
+#     close(f)
+# end
 
 
 
