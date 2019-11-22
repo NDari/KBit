e217dc37abafe6b5b7846f417ae80d4f5d4a46b4
Naseer Dari <naseerdari01@gmail.com>
2019-11-21 23:05:20 -0500

    revert extract page, add extract text

--- src/extract_articles.jl
+++ src/extract_articles.jl
@@ -13,6 +13,38 @@ function extract(file)
 end
 
 function extract_page(stream)
+    nf = "<mediawiki>\n"
+    page_found = false
+    title_regex = r"<title>(.*)</title>"
+    title = nothing
+    for line in eachline(stream)
+        if page_found
+            if occursin(title_regex, line)
+                title = match(title_regex, line)[1]
+                title = replace(title, r"/| " => "_")
+            end
+            if occursin(r"</page>", line)
+                nf *= line * "\n"
+                break
+            end
+            nf *= line * "\n"
+            continue
+        end
+        if !page_found && occursin(r"<page>", line)
+            nf *= line * "\n"
+            page_found = true
+        end
+    end
+    if nf === "<mediawiki>/n"
+        return
+    end
+    nf *= "</mediawiki>"
+    f = open("./data/$title.xml", "w")
+    write(f, nf)
+    close(f)
+end
+
+function extract_text(stream)
     nf = ""
     text_found = false
     text_regex = r"<text xml:space=\"preserve\">"
@@ -42,7 +74,6 @@ function extract_page(stream)
     end
     return nf
 end
-
 
 # function par_extract_page(mmap_file)
 #     chunck_size = length(mmap_file) ÷ nthreads()
