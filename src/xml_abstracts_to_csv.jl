7938b9b86ecaa978512c67113bb56a80b17af906
Naseer Dari <naseerdari01@gmail.com>
2019-12-04 22:09:02 -0500

    working xml converter

--- src/xml_abstracts_to_csv.jl
+++ src/xml_abstracts_to_csv.jl
@@ -3,23 +3,40 @@ import Fire: @main
 function convert(xmlfile::AbstractString, csvfile::AbstractString = "wiki_abstracts.csv")
     out = open(csvfile, "w")
     instream = open(xmlfile, "r")
-    write(out, "title,abstract")
-    for i = 1:1000
-        write(out, get_pair(instream))
+    write(out, "title,abstract\n")
+    while !eof(instream)
+        res = get_line(instream)
+        if res === ""
+            continue
+        end
+        write(out, res)
     end
     close(out)
     close(instream)
 end
 
-function get_pair(stream)
+function get_line(stream)
     while !eof(stream)
         if readline(stream) === "<doc>"
             break
         end
     end
-    title = match(r"<title>(.*)</title>", readline(stream))[1]
-    abstct = match(r"<abstract>(.*)</abstract>", readline(stream))[1]
-    return "$title,$abstract"
+    if eof(stream)
+        return ""
+    end
+    title = match(r"<title>Wikipedia: (.*)</title>", readline(stream))
+    if title == nothing
+        return ""
+    end
+    title = title[1]
+    title = replace(title, "\"" => "'")
+    abstct = match(r"<abstract>(.*)</abstract>", readline(stream))
+    if abstct == nothing
+        return ""
+    end
+    abstct = abstct[1]
+    abstct = replace(abstct, "\"" => "'")
+    return "\"$title\",\"$abstct\"\n"
 end
 
 
