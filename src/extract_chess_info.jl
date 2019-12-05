76df6962e2f6ebc40398638cb3ce0d32a5023f5f
Naseer Dari <naseerdari01@gmail.com>
2019-12-05 13:27:31 -0500

    extract chess data from the new csv

--- src/extract_chess_info.jl
+++ src/extract_chess_info.jl
@@ -1,19 +1,16 @@
-#!/usr/bin/env julia
-
 import DataFrames: DataFrame
 import CSV
 import Fire: @main
-using DataFramesMeta
+import DataFramesMeta: @where
 
 
 @main function exec(raw_csv::AbstractString)
   df = DataFrame(CSV.read(raw_csv))
   res = @where df chess_data.(:abstract)
-  fixed = @byrow! res :title = replace(:title, "Wikipedia: " => "")
-  CSV.write("../data/chess.csv", fixed)
+  CSV.write("data/chess.csv", res)
 end
 
 function chess_data(x)
-  typeof(x) == String ? occursin(r" (C|c)hess(\s|\.)", x) : false
+    typeof(x) == String ? occursin(r" [Cc]hess[\s\.]", x) : false
 end
 
