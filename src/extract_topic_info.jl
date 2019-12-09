62a41cce811ca5afeb2b37de2639510984114046
Naseer Dari <naseerdari01@gmail.com>
2019-12-09 09:53:59 -0500

    move chess to topic. use stopwords

---
+++ src/extract_topic_info.jl
@@ -0,0 +1,22 @@
+import DataFrames: DataFrame
+import CSV
+import Fire: @main
+import DataFramesMeta: @where, @byrow!, @newcol
+import Languages
+
+
+@main function exec(raw_csv::AbstractString)
+  df = DataFrame(CSV.read(raw_csv))
+  res = @where df topic_data.(:abstract)
+  CSV.write("data/cancer.csv", res)
+end
+
+function topic_data(x)
+    typeof(x) == String ? occursin(r" [Cc]ancer[\s\.]", x) : false
+end
+
+function remove_stop_words(abstrct, stop_words)
+    join(filter(w -> ∉(w, stop_words), split(abstrct)), " ")
+end
+
+
