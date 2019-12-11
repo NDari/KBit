c07e33072d04e3e8434d52f7c379564f794ddd3f
Naseer Dari <naseerdari01@gmail.com>
2019-12-11 15:37:49 -0500

    page extraction again

--- src/extract_articles.jl
+++ src/extract_articles.jl
@@ -2,6 +2,7 @@ import TranscodingStreams: TranscodingStream
 import CodecBzip2: Bzip2Decompressor
 import Mmap: mmap
 import ProgressMeter: @showprogress
+import Lazy: @>
 
 function extract(file)
     fs = mmap(open(file))
@@ -14,99 +15,61 @@ end
 
 function extract_text(stream)
     nf = ""
-    text_found = false
-    text_regex = r"<text xml:space=\"preserve\">"
+    title = ""
     title_regex = r"<title>(.*)</title>"
-    for line in eachline(stream)
-        if !text_found
-            if occursin(text_regex, line)
-                line = replace(line, text_regex => "")
-                if occursin(r"</text>", line)
-                    nf *= replace(line, r"</text>" => "") * "\n"
-                    break
-                else
-                    nf *= line * "\n"
-                end
-                text_found = true
-            elseif occursin(title_regex, line)
-                title = match(title_regex, line)[1]
-                nf *= "# " * replace(title, r"/| " => "_") * "\n"
-            end
+    while !eof(stream)
+        line = readline(stream)
+        if !occursin(title_regex, line)
             continue
-        end
-        if occursin(r"</text>", line)
-            nf *= replace(line, r"</text>" => "") * "\n"
+        else
+            title = match(title_regex, line)[1]
+            nf *= "# " * replace(title, r"/| " => "_") * "\n"
             break
         end
-        nf *= line * "\n"
     end
-    return nf
-end
-
-function extract_page(stream)
-    nf = "<mediawiki>\n"
-    page_found = false
-    title_regex = r"<title>(.*)</title>"
-    title = nothing
-    for line in eachline(stream)
-        if page_found
-            if occursin(title_regex, line)
-                title = match(title_regex, line)[1]
-                title = replace(title, r"/| " => "_")
+    text_regex = r"<text xml:space=\"preserve\">"
+    text_found = false
+    page_end = r"</page>"
+    while !eof(stream)
+        line = readline(stream)
+        if occursin(page_end, line)
+            return "", ""
+        end
+        if !text_found
+            if !occursin(text_regex, line)
+            else
+                text_found = true
             end
-            if occursin(r"</page>", line)
-                nf *= line * "\n"
+            continue
+        else
+            if occursin(r"</text>", line)
+                nf *= replace(line, r"</text>" => "") * "\n"
                 break
             end
             nf *= line * "\n"
-            continue
-        end
-        if !page_found && occursin(r"<page>", line)
-            nf *= line * "\n"
-            page_found = true
         end
     end
-    if nf === "<mediawiki>/n"
-        return
+    while !eof(stream)
+        line = readline(stream)
+        if occursin(page_end, line)
+            break
+        end
     end
-    nf *= "</mediawiki>"
-    f = open("./data/$title.xml", "w")
-    write(f, nf)
-    close(f)
+    return title, nf
 end
 
+function clean_text(nf)
+    @> nf begin
+        replace(r"{{(.*)}}" => "")
+        replace(r"\[\[(.*):(.*)\]\]" => "")
+        replace(r"\'\'\'(.*)\'\'\'" => s"**\1**")
+        replace(r"\'\'(.*)\'\'" => s"*\1*")
+        replace(r"\n====\s?(.*)====\n" => s"\n#### \1\n")
+        replace(r"\n===\s?(.*)===\n" => s"\n### \1\n")
+        replace(r"\n==\s?(.*)==\n" => s"\n## \1\n")
+    end
+end
 
-# function par_extract_page(mmap_file)
-#     chunck_size = length(mmap_file) ÷ nthreads()
-#     idx_start = threadid() === 1 ? 1 : chunck_size * (threadid() - 1)
-#     idx_end = threadid() === nthreads() ? length(mmap_file) : chunck_size * threadid()
-#     stream = TranscodingStream(Bzip2Decompressor(), IOBuffer(fs[idx_start:idx_end]))
-#     nf = "<mediawiki>\n"
-#     page_found = false
-#     title_regex = r"<title>(.*)</title>"
-#     title = nothing
-#     for line in eachline(stream)
-#         if page_found
-#             if occursin(title_regex, line)
-#                 title = match(title_regex, line)[1]
-#             end
-#             if occursin(r"</page>", line)
-#                 nf *= line * "\n"
-#                 break
-#             end
-#             nf *= line * "\n"
-#             continue
-#         end
-#         if !page_found && occursin(r"<page>", line)
-#             nf *= line * "\n"
-#             page_found = true
-#         end
-#     end
-#     nf *= "</mediawiki>"
-#     f = open("./data/$title.xml", "w")
-#     write(f, nf)
-#     close(f)
-# end
 
 
 
